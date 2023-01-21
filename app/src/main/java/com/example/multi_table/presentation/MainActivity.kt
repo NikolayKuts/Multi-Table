a8bef90287a901638afcee87264d8776fab4ca06
NikolayKuts <survivenik@gmail.com>
2023-01-21 13:34:09 +0200

    Simplify MainActiity

--- app/src/main/java/com/example/multi_table/presentation/MainActivity.kt
+++ app/src/main/java/com/example/multi_table/presentation/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toArgb
-import androidx.lifecycle.lifecycleScope
 import com.example.multi_table.presentation.theme.MainTheme
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
@@ -20,49 +18,52 @@ class MainActivity : ComponentActivity() {
 
         lifecycle.addObserver(viewModel)
 
-        lifecycleScope.launch {
-            viewModel.state.collect { state ->
-                setContent {
-                    MainTheme {
+        viewModel.state.collectWhenStarted(lifecycleOwner = this) { state ->
+            setContent {
+                MainTheme {
+                    Surface {
                         window.statusBarColor = MainTheme.colors.materialColors.primary.toArgb()
-
-                        Surface() {
-                            when (state) {
-                                is MainState.StartState -> {
-                                    BeginningView(
-                                        onStartButtonClick = {
-                                            viewModel.sendEvent(event = MainEvent.NextExpression)
-                                        }
-                                    )
-                                }
-                                is MainState.QuestionedState -> {
-                                    QuestionView(
-                                        expression = state.expression,
-                                        time = state.time,
-                                        onResultButtonClick = {
-                                            viewModel.sendEvent(event = MainEvent.ExpressionResult)
-                                        }
-                                    )
-                                }
-                                is MainState.ResultState -> {
-                                    if (state.expression != null) {
-                                        ResultView(
-                                            expression = state.expression,
-                                            time = state.time,
-                                            onWrongButtonClick = {
-                                                viewModel.sendEvent(event = MainEvent.WrongAnswer)
-                                            },
-                                            onNextButtonClick = {
-                                                viewModel.sendEvent(event = MainEvent.NextExpression)
-                                            }
-                                        )
-                                    }
-                                }
-                            }
-                        }
+                        ScreenState(state = state)
                     }
                 }
             }
         }
+    }
+
+    @Composable
+    private fun ScreenState(state: MainState) {
+        when (state) {
+            is MainState.StartState -> {
+                BeginningView(
+                    onStartButtonClick = {
+                        viewModel.sendEvent(event = MainEvent.NextExpression)
+                    }
+                )
+            }
+            is MainState.QuestionedState -> {
+                QuestionView(
+                    expression = state.expression,
+                    time = state.time,
+                    onResultButtonClick = {
+                        viewModel.sendEvent(event = MainEvent.ExpressionResult)
+                    }
+                )
+            }
+            is MainState.ResultState -> {
+                if (state.expression != null) {
+                    ResultView(
+                        expression = state.expression,
+                        time = state.time,
+                        onWrongButtonClick = {
+                            viewModel.sendEvent(event = MainEvent.WrongAnswer)
+                        },
+                        onNextButtonClick = {
+                            viewModel.sendEvent(event = MainEvent.NextExpression)
+                        }
+                    )
+                }
+            }
+        }
+
     }
 }
