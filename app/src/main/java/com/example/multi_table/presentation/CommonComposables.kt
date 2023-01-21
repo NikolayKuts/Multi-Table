87b6dbc82f0a7335ef842317da3a5ccf0550cb07
NikolayKuts <survivenik@gmail.com>
2023-01-21 14:26:40 +0200

    Refactor the structure of QuestionView

--- app/src/main/java/com/example/multi_table/presentation/CommonComposables.kt
+++ app/src/main/java/com/example/multi_table/presentation/CommonComposables.kt
@@ -1,14 +1,19 @@
 package com.example.multi_table.presentation
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.multi_table.R
+import com.example.multi_table.domain.entities.MultiplicationExpression
+import com.example.multi_table.presentation.theme.MainTheme
 
 
 @Composable
@@ -20,4 +25,61 @@ fun AppButton(@StringRes textId: Int, onClick: () -> Unit) {
     ) {
         Text(text = stringResource(textId))
     }
+}
+
+fun Modifier.buttonPadding(): Modifier = this.padding(start = 24.dp, end = 24.dp, bottom = 24.dp)
+
+@Composable
+fun BoxScope.TimeView(seconds: Int, millis: Int) {
+    Row(
+        modifier = Modifier
+            .align(alignment = Alignment.TopCenter)
+            .padding(top = (LocalConfiguration.current.screenHeightDp / 16).dp)
+    ) {
+        Text(
+            text = seconds.toString(),
+            style = MainTheme.typographies.timerTextStyle
+        )
+        Text(
+            text = stringResource(R.string.text_colon),
+            modifier = Modifier
+                .padding(start = 4.dp, end = 4.dp)
+                .offset(y = (-2.5).dp),
+            style = MainTheme.typographies.timerTextStyle
+        )
+        Text(
+            text = millis.toString(),
+            style = MainTheme.typographies.timerTextStyle
+        )
+    }
+}
+
+@Composable
+fun ExpressionElements(
+    expression: MultiplicationExpression,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ExpressionElement(text = expression.multiplicand.toString())
+
+        Text(
+            text = stringResource(R.string.text_multiplication_sign),
+            modifier = Modifier.padding(8.dp),
+            style = MainTheme.typographies.multiplicationSignTextStyle
+        )
+
+        ExpressionElement(text = expression.multiplier.toString())
+    }
+}
+
+@Composable
+private fun ExpressionElement(text: String) {
+    Text(
+        text = text,
+        modifier = Modifier.padding(8.dp),
+        style = MainTheme.typographies.expressionTextStyle
+    )
 }
--- app/src/main/java/com/example/multi_table/presentation/MainView.kt
+++ app/src/main/java/com/example/multi_table/presentation/MainView.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.multi_table.R
@@ -17,36 +16,6 @@ import com.example.multi_table.domain.entities.MultiplicationExpression
 import com.example.multi_table.presentation.theme.MainTheme
 import kotlinx.coroutines.flow.StateFlow
 
-@Composable
-fun QuestionView(
-    expression: MultiplicationExpression,
-    time: StateFlow<Timer.Time>,
-    onResultButtonClick: () -> Unit,
-) {
-    val timeState = time.collectAsState()
-
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        TimeView(seconds = timeState.value.seconds, millis = timeState.value.millis)
-
-        ExpressionElements(
-            expression = expression,
-            modifier = Modifier.align(alignment = Alignment.Center)
-        )
-
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(alignment = Alignment.BottomCenter)
-                .padding(start = 24.dp, end = 24.dp, bottom = 24.dp),
-            elevation = ButtonDefaults.elevation(defaultElevation = 4.dp),
-            onClick = onResultButtonClick
-        ) {
-            Text(text = stringResource(R.string.button_text_result))
-        }
-    }
-}
 
 @Composable
 fun ResultView(
@@ -108,36 +77,6 @@ private fun ExpressionResultElements(expression: MultiplicationExpression) {
 }
 
 @Composable
-private fun ExpressionElements(
-    expression: MultiplicationExpression,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        ExpressionElement(text = expression.multiplicand.toString())
-
-        Text(
-            text = stringResource(R.string.text_multiplication_sign),
-            modifier = Modifier.padding(8.dp),
-            style = MainTheme.typographies.multiplicationSignTextStyle
-        )
-
-        ExpressionElement(text = expression.multiplier.toString())
-    }
-}
-
-@Composable
-fun ExpressionElement(text: String) {
-    Text(
-        text = text,
-        modifier = Modifier.padding(8.dp),
-        style = MainTheme.typographies.expressionTextStyle
-    )
-}
-
-@Composable
 private fun ResultViewButton(text: String, onClick: () -> Unit) {
     Button(
         modifier = Modifier.fillMaxWidth(),
@@ -145,30 +84,5 @@ private fun ResultViewButton(text: String, onClick: () -> Unit) {
         elevation = ButtonDefaults.elevation(defaultElevation = 4.dp)
     ) {
         Text(text = text)
-    }
-}
-
-@Composable
-private fun BoxScope.TimeView(seconds: Int, millis: Int) {
-    Row(
-        modifier = Modifier
-            .align(alignment = Alignment.TopCenter)
-            .padding(top = (LocalConfiguration.current.screenHeightDp / 16).dp)
-    ) {
-        Text(
-            text = seconds.toString(),
-            style = MainTheme.typographies.timerTextStyle
-        )
-        Text(
-            text = stringResource(R.string.text_colon),
-            modifier = Modifier
-                .padding(start = 4.dp, end = 4.dp)
-                .offset(y = (-2.5).dp),
-            style = MainTheme.typographies.timerTextStyle
-        )
-        Text(
-            text = millis.toString(),
-            style = MainTheme.typographies.timerTextStyle
-        )
     }
 }
